d44edadc4fafcc12e6655a9041b4e787194ea3f4
Deshra K <115415485+deshrak@users.noreply.github.com>
2023-08-04 11:30:26 +0530

    Update desh.java

--- desh.java
+++ desh.java
@@ -2,9 +2,7 @@ import java.util.Scanner;
 
 public class desh {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number of terms in the Fibonacci series: ");
-        int n = input.nextInt();
+        int n = 10;
 
         System.out.println("Fibonacci Series:");
         for (int i = 0; i < n; i++) {
